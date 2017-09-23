769c02ec68f888a15260c243d0c8d4640c0cea8d
Wonderful <wonderful@Bests-iMac.local>
2017-09-23 16:59:56 +0200

    chrome_install shell script attached for ubuntu

---
+++ chrome_install.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+sudo apt-get update & sudo apt-get -y upgrade
+sudo apt-get install libxss1 libappindicator1 libindicator7
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+
+sudo dpkg -i google-chrome*.deb
+sudo apt-get install -f
+
+sudo apt-get install xvfb
+
+sudo apt-get install unzip
+
+wget -N http://chromedriver.storage.googleapis.com/2.26/chromedriver_linux64.zip
+unzip chromedriver_linux64.zip
+chmod +x chromedriver
+
+sudo mv -f chromedriver /usr/local/share/chromedriver
+sudo ln -s /usr/local/share/chromedriver /usr/local/bin/chromedriver
+sudo ln -s /usr/local/share/chromedriver /usr/bin/chromedriver
+
+sudo apt-get install python-pip
+
+sudo apt-get install python-virtualenv
+virtualenv selenium_venv
+source selenium_venv/bin/activate
+
+pip install pyvirtualdisplay selenium
